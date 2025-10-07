b0e4993e9a20e630d29f36d63f53896789b5e812
Verónica García <veronicagaiglesias@gmail.com>
2025-10-07 20:14:57 +0200

    feat: creacion de archivos para componentes header y footer

---
+++ ui/js/loadHeader.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("ui/components/header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    })
+    .catch(err => console.error("No se pudo cargar el header:", err));
+});
+
