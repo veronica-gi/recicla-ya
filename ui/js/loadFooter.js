6275909f95b3a89c1b9af994ecb234d636687152
Verónica García <veronicagaiglesias@gmail.com>
2025-10-08 18:04:01 +0200

    fix: corrección de rutas de header y footer

--- ui/js/loadFooter.js
+++ ui/js/loadFooter.js
@@ -1,3 +1,3 @@
-fetch('ui/components/footer.html')
+fetch('./ui/components/footer.html')
   .then(res => res.text())
   .then(html => document.getElementById('footer').innerHTML = html);
--- ui/js/loadHeader.js
+++ ui/js/loadHeader.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("ui/components/header.html")
+  fetch("./ui/components/header.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("header").innerHTML = data;
